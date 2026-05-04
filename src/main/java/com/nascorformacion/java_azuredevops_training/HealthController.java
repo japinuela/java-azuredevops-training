a3ba975a7c0f7a147145a637c736a9a538cab3d6
Juan A. Piñuela <juan.pinuela@universidadeuropea.es>
2026-05-04 12:45:45 +0200

    feat: quitar  TODO

--- src/main/java/com/nascorformacion/java_azuredevops_training/HealthController.java
+++ src/main/java/com/nascorformacion/java_azuredevops_training/HealthController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HealthController {
-    //TODO Ejemplo de comentario
     private final GreetingService greetingService;
 
     public HealthController(GreetingService greetingService) {
